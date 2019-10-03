981eb59a22922d278d2e4e0c499d7c4ea38007b5
Michael Pruss <michael.pruss@carleton.ca>
2019-10-03 19:26:46 -0400

    Update

--- lab4/intSender.py
+++ lab4/intSender.py
@@ -15,10 +15,8 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 port = int(textport)
 server_address = (host, port)
 
-while 1:
-    for i in range(0, int(numMessage)):
-        number = str(random.randint(1, 100))
-        s.sendto(number.encode('utf-8'), server_address)
+for i in range(0, int(numMessage)):
+    number = str(random.randint(1, 100))
+    s.sendto(number.encode('utf-8'), server_address)
 
-s.shutdown(1)
 
